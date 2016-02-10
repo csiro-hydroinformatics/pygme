2656816e580526fd14eb76a579743d60097b7419
Julien Lerat <julien.lerat@gmail.com>
2016-02-10 21:09:13 +1100

    knn - work in progress

--- pygme/models/c_knn.c
+++ pygme/models/c_knn.c
@@ -1,12 +1,12 @@
-#include "c_knndaily.h"
+#include "c_knn.h"
 
 /*
-* Daily model : 
-* Calculation of nearest neighbours according the methodology detailed  
-* by Lall, U., Sharma, A., 1996. A nearest neighbour bootstrap for time 
+* Daily model :
+* Calculation of nearest neighbours according the methodology detailed
+* by Lall, U., Sharma, A., 1996. A nearest neighbour bootstrap for time
 * series resampling. Water Resources Research 32 (3), 679–693.
-* 
-* Inputs 
+*
+* Inputs
 *   NBK   Number of nearest neighbours to consider
 *   WINS  Temporal window to restrain the search for nearest neighbours
 *   DMAT  Matrix of feature vectors for each days (nval x nvar)
@@ -14,12 +14,12 @@
 *   SMAT  Data to resample
 *   NNdep Initial day
 *   RND   Random number vector
-*   
+*
 * Outputs
 *   KNNSIM Resampled data
 */
 
-int c_knndaily_run(int nparams, int nval, int nvar, int nrand, 
+int c_knn_run(int nparams, int nval, int nvar, int nrand,
     double * params,
     double * weights,
     double * var,
@@ -27,16 +27,22 @@ int c_knndaily_run(int nparams, int nval, int nvar, int nrand,
     double * outputs)
 {
     int ierr;
-    int i, winlag, nbk, nyears;
+    int i, k, winlag, nbk, nyears;
 
     double sum;
     double kernel[KNN_NKERNEL_MAX];
+    double distance[KNN_NVAR_MAX];
+
+    if(nvar > KNN_NVAR_MAX)
+        return 444;
 
     /* Half temporal window selection */
     winlag = (int)params[0];
 
     /* Number of neighbours */
     nbk = (int)params[1];
+    if(nbk >= KNN_NKERNEL_MAX)
+        return 444;
 
     /* Number of years in input matrix */
     nyears = (int)(nval/365.25)+1;
@@ -44,6 +50,7 @@ int c_knndaily_run(int nparams, int nval, int nvar, int nrand,
     /* Normalise weights */
     sum = 0;
     for(i=0; i<nval; i++) sum += fabs(weights[i]);
+
     if(sum <= 0)
         return 444;
 
@@ -56,13 +63,21 @@ int c_knndaily_run(int nparams, int nval, int nvar, int nrand,
     kernel[0] = 1./sum;
     for(i=1; i<nbk; i++) kernel[i] = kernel[i-1] + 1./(double)(i+1)/sum;
 
+    /* resample */
+    for(i=0; i<nval; i++)
+    {
+        /* reset distance */
+        for(k=0; k<nvar; k++)
+            distance[k] = 0;
+
+    }
 
     return ierr;
 }
 
 int knn_sim(int nval, int nvar,
-	int * _nrand, double * NBK, 
-  	double * WINS, double * DMAT, double * WEI, double * DMATSELEC, 
+	int * _nrand, double * NBK,
+  	double * WINS, double * DMAT, double * WEI, double * DMATSELEC,
   	double * NNdep, double * RND,double *PRINTMESSAGE, double * KNNSIM_NN)
 {
     int nbk,dmatCol,nnCol,day,day2,NbAnn,ann,debDay,finDay,nnSelec;
@@ -74,16 +89,16 @@ int knn_sim(int nval, int nvar,
     // Error if the number of days is greater than SIZEMAX
     // or if the number of features is greater than NBFEATMAX
     if(nval*nvar>=SIZEMAX){return;}
-    
+
 	nrand = _nrand[0];
 
     nbk=(int)(NBK[0]);
     if(nbk>=MAXCOL){return;}
-    
+
     // Half temporal window
     winlag=WINS[0]/2;if(winlag<=0){return;}
 
-    // Number of years in DMAT and SMAT 
+    // Number of years in DMAT and SMAT
     NbAnn = (int)(nval/365.25)+1;
 
 	// Print messages
@@ -113,11 +128,11 @@ int knn_sim(int nval, int nvar,
     if(DMATSELEC[(int)(NNdep[0])]<=0 && print!=0.0 ){
 		printf("CAUTION ! The starting day is not a selected day >> problem with the resampling algo.");
 	}
-    
+
     /*----- Initialisation des caractéristiques ----------*/
     nnSelec=(int)(NNdep[0]);if((nnSelec<0)|(nnSelec>=nval)){nnSelec=0;}
     for(dmatCol=0;dmatCol<nvar;dmatCol++){CARAC[dmatCol]=DMAT[nnSelec+nval*dmatCol];}
-    
+
     /*----- Resampling kernel ----------*/
     sumkern=0;for(nnCol=0;nnCol<nbk;nnCol++){sumkern+=1/(double)(nnCol+1);}
     KERN[0]=1/sumkern;
@@ -129,12 +144,12 @@ int knn_sim(int nval, int nvar,
       if(print!=0.0) printf("%5.3f ",KERN[nnCol]);
     }
     if(print!=0.0) printf("\n\tLine nb:\n\t");
-        
+
     /*----- Resampled data generation ----------*/
-    for(day=0;day<nrand;day++){    
+    for(day=0;day<nrand;day++){
         if(fmod(day,1000)==0 && print!=0.0){printf("%5d ",day);}
         if(fmod(day,8000)==0 && print!=0.0){printf("\n\t");}
-                
+
         // initialize the distance vector
         for(nnCol=0;nnCol<nbk;nnCol++){DIST[nnCol]=MAXVALDIST;}
 
@@ -146,10 +161,10 @@ int knn_sim(int nval, int nvar,
             debDay=(int)(ann*365.25-winlag+fmod(nnSelec,365.25));
             finDay=(int)(ann*365.25+winlag+fmod(nnSelec,365.25));
             if(debDay<0){debDay=0;}
-            if(finDay<0){finDay=0;}   
-            if(debDay>=nval){debDay=nval-1;} 
-            if(finDay>=nval){finDay=nval-1;}    
-                    
+            if(finDay<0){finDay=0;}
+            if(debDay>=nval){debDay=nval-1;}
+            if(finDay>=nval){finDay=nval-1;}
+
             for(day2=debDay;day2<finDay;day2++){
 
                 // Parcours la base de données en ne retenant que les jours sélectionnés
@@ -165,7 +180,7 @@ int knn_sim(int nval, int nvar,
                     nnClass=0;
                     while((DISTtemp>DIST[nnClass])&(nnClass<=nbk)){nnClass++;}
 
-                    // if yes, rearrange DIST and NNliste vectors 
+                    // if yes, rearrange DIST and NNliste vectors
                     if(nnClass<nbk){
                         for(nnCol=nbk-1;nnCol>=nnClass+1;nnCol--){
                             DIST[nnCol]=DIST[nnCol-1];NNliste[nnCol]=NNliste[nnCol-1];
@@ -173,10 +188,10 @@ int knn_sim(int nval, int nvar,
                         NNliste[nnClass]=day2;DIST[nnClass]=DISTtemp;
                     }
 
-                } 
+                }
             }
         }
-        
+
         // Random number
         rnd=RND[day];if((rnd<0)|(rnd>1)){rnd=0.5;}
 
@@ -184,15 +199,15 @@ int knn_sim(int nval, int nvar,
         nnCol=0;while((rnd>KERN[nnCol])&(nnCol<nbk)){nnCol++;}
         nnSelec = (int)(NNliste[nnCol]+1);
         if(nnSelec>=nval){nnSelec=nval-1;}
-        
+
         // Stockage des caractéristiques du jour
         for(dmatCol=0;dmatCol<nvar;dmatCol++){CARAC[dmatCol]=DMAT[nnSelec+nval*dmatCol];}
 
         // Attribution of the data to KNNSIM matrix
         KNNSIM_NN[day]=(double)(nnSelec);
         //printf(" >> %5.0f\n",KNNSIM_NN[day]);
-        
+
     } // End of the simulation
-    
-     if(print!=0.0) printf("\n\n");    
+
+     if(print!=0.0) printf("\n\n");
 }
--- pygme/models/c_knn.h
+++ pygme/models/c_knn.h
@@ -11,8 +11,11 @@
 /* Maximum size of resampling kernel */
 #define KNN_NKERNEL_MAX 150
 
+/* Maximum number of variables to evaluate distance */
+#define KNN_NVAR_MAX 10
 
-int c_knndaily_run(int nparams, int nval, int nvar, int nrand, 
+
+int c_knn_run(int nparams, int nval, int nvar, int nrand,
     double * params,
     double * weights,
     double * var,
