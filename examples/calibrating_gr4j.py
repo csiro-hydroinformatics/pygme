e323daefa52ab478c1f8a7fe4947f864ce189f5a
Julien Lerat <julien.lerat@gmail.com>
2018-11-20 21:10:46 +1100

    fixed example

--- examples/calibrating_gr4j.py
+++ examples/calibrating_gr4j.py
@@ -1,47 +1,57 @@
-import os
+import os, sys
 import numpy as np
+import pandas as pd
 
 from pygme.calibration import ObjFunBCSSE
 from pygme.models.gr4j import GR4J, CalibrationGR4J
+
 from hydrodiy.stat import metrics
 
+# Add access to test data
+source_file = os.path.abspath(__file__)
+froot = os.path.join(os.path.dirname(source_file), '..')
+sys.path.append(os.path.join(froot, 'tests'))
+import testdata
+
+# initialise GR4J
 gr = GR4J()
 warmup = 365*6
 
-# Open data file
-print('Reading data')
-siteid = 1
-filename = os.path.abspath(__file__)
-FHERE = os.path.dirname(filename)
-fts = '{0}/../tests/data/GR4J_timeseries_{1:02d}.csv'.format(FHERE, siteid)
-data = np.loadtxt(fts, delimiter=',')
+# Get data
+i = 10
+data = testdata.read('GR4J_timeseries_{0:02d}.csv'.format(i+1), \
+                        source='output', has_dates=False)
+inputs = np.ascontiguousarray(\
+                data.loc[:, ['Precip', 'PotEvap']], \
+                np.float64)
 
-# Get input and obs data
-inputs = data[:, [1, 2]]
-inputs = np.ascontiguousarray(inputs, np.float64)
-obs = data[:, 3]
+obs = data.Qobs.values
 
 # Define calibration period
+print('Define calibration period')
 idx_cal = np.arange(len(inputs))>=warmup
 idx_cal = idx_cal & (obs >= 0)
 
 # Calibrate
 print('Calibrating GR4J')
 calib = CalibrationGR4J(ObjFunBCSSE())
-calparams, _, _ = calib.workflow(obs, inputs, ical=idx_cal)
+calib.workflow(obs, inputs, ical=idx_cal)
 
-# Get model parameters
-gr = calib.model
 params = gr.params.values
+print('Calibrated parameters:')
+print('\t' + '\n\t'.join(['X{0} = {1:0.1f}'.format(i+1, params[i]) \
+                for i in range(4)]))
 
 # Run model
 print('Running GR4J and compute stats')
+gr = calib.model
 gr.inputs = inputs
 gr.run()
 sim = gr.outputs[:, 0]
 
-# Mode statistics
-bias = metrics.bias(obs, sim)
-nse = metrics.nse(obs, sim)
+# Statistics over calibration period
+obsc, simc = obs[idx_cal], sim[idx_cal]
+bias = metrics.bias(obsc, simc)
+nse = metrics.nse(obsc, simc)
 print(' bias = {0:.3f}\n NSE  = {1:.3f}'.format(bias, nse))
 
