8506534c97e33b7936810bbf9b72a61184827838
Julien Lerat <julien.lerat@csiro.au>
2021-08-31 23:58:05 +1000

    fix: Fixedimportlib in __init__.

--- pygme/__init__.py
+++ pygme/__init__.py
@@ -1,17 +1,17 @@
-import imp
+import importlib
 
 def has_c_module(name, raise_error=True):
-    try:
-        name = f"c_pygme_{name}"
-        fp, pathname, description = imp.find_module(name)
+    name = f"c_pygme_{name}"
+    out = importlib.util.find_spec(name)
+
+    if not out is None:
         return True
-    except ImportError:
+    else:
         if raise_error:
             raise ImportError(f"C module c_pygme_{name} is "+\
                 "not available, please run python setup.py build")
         else:
             return False
-
 
 from ._version import get_versions
 __version__ = get_versions()['version']
