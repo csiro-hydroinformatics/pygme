b8875c6bd567d100695fa8cbfb9d70e20b189bd5
Julien Lerat <julien.lerat@gmail.com>
2022-02-04 12:39:43 +1100

    feat: expanded paramslib to multiple models [ci skip]

--- paramslib/scripts/calmodel.py
+++ paramslib/scripts/calmodel.py
@@ -18,17 +18,20 @@ from pathlib import Path
 
 import numpy as np
 import pandas as pd
+from scipy.optimize import fmin_bfgs
 
-from hydrodiy.io import csv, iutils, hyruns
-from hydrodiy.stat import metrics, sutils
+from hydrodiy.io import csv, iutils
+from hydrodiy.io.hyruns import get_batch, OptionManager
+from hydrodiy.stat import metrics, sutils, transform
 
 from datasets import Dataset
 
-from pygme.models import sac15
+from pygme.models import sac15, wapaba, gr6j
 
 import importlib
 importlib.reload(sac15)
-
+importlib.reload(wapaba)
+importlib.reload(gr6j)
 
 from tqdm import tqdm
 
@@ -42,10 +45,8 @@ parser = argparse.ArgumentParser(\
 parser.add_argument("-v", "--version", \
                     help="Version number", \
                     type=int, required=True)
-parser.add_argument("-s", "--sitepattern", help="Site selection pattern", \
-                    type=str, default="")
-parser.add_argument("-i", "--ibatch", help="Batch process number", \
-                    type=int, default=-1)
+parser.add_argument("-t", "--taskid", help="Task id", \
+                    type=int, required=True)
 parser.add_argument("-n", "--nbatch", help="Number of batch processes", \
                     type=int, default=20)
 parser.add_argument("-p", "--progress", help=" Show progress", \
@@ -54,66 +55,101 @@ parser.add_argument("-o", "--overwrite", help="Overwrite", \
                     action="store_true", default=False)
 args = parser.parse_args()
 
+taskid = args.taskid
 version = args.version
-ibatch = args.ibatch
-nbatch = args.nbatch
 progress = args.progress
-sitepattern = args.sitepattern
 overwrite = args.overwrite
-
-# 10 years of warmup
-warmup = int(10*365.25)
+nbatch = args.nbatch
 
 # Large params lib
 nparamslib = 30000
+
+# Get option manager
+opm = OptionManager()
+models = ["sac15", "gr6j", "wapaba"]
+batches = np.arange(nbatch)
+opm.from_cartesian_product(model=models, batch=batches)
+
+# Get task
+task = opm.get_task(taskid)
+model_name = task.model
+batch = task.batch
 
 #----------------------------------------------------------------------
 # Folders
 #----------------------------------------------------------------------
 source_file = Path(__file__).resolve()
 froot = source_file.parent.parent
-fout = froot / "outputs" / f"calsac_v{version}"
+fout = froot / "outputs" / f"calmodel_{model_name}_v{version}"
 fout.mkdir(exist_ok=True, parents=True)
 
-flogs = froot / "logs" / "calsac"
+flogs = froot / "logs" / "calmodel"
 flogs.mkdir(exist_ok=True, parents=True)
 basename = source_file.stem
-flog = flogs / f"calsac_TASK{ibatch}_V{version}.log"
+flog = flogs / f"calsac_TASK{taskid}_V{version}_M{model_name}_B{batch}.log"
 LOGGER = iutils.get_logger(basename, flog=flog)
 
+LOGGER.info(f"taskid: {taskid}")
+LOGGER.info(f"model: {model_name}")
 LOGGER.info(f"version: {version}")
-LOGGER.info(f"ibatch:  {ibatch}")
+LOGGER.info(f"batch:  {batch}")
 
 #----------------------------------------------------------------------
 # Get data
 #----------------------------------------------------------------------
 dset = Dataset("OZDATA", "1.0")
-sites_all = dset.get_sites()
+sites = dset.get_sites()
 
-# Select sites
-sites = sites_all
-if not sitepattern == "":
-    idx = sites_all.index.str.findall(sitepattern).astype(bool)
-    sites = sites_all.loc[idx, :]
-else:
-    if ibatch >=0:
-        idx = hyruns.get_batch(sites_all.shape[0], nbatch, ibatch)
-        sites = sites_all.iloc[idx, :]
+# select site from batch
+isites = get_batch(len(sites), nbatch, batch)
+sites = sites.iloc[isites, :]
 
 #----------------------------------------------------------------------
 # Process
 #----------------------------------------------------------------------
 
-#load params lib from previous versions
-model = sac15.SAC15()
-if version == 1:
-    LOGGER.info(f"Retrieve parameter library from package")
+# Load model objects
+if model_name == "sac15":
+    model = sac15.SAC15()
+    CalibrationObject = sac15.CalibrationSAC15
     means = sac15.SAC15_TMEAN
     cov = sac15.SAC15_TCOV
+    true2trans = sac15.sac15_true2trans
+    trans2true = sac15.sac15_trans2true
+    timestep = "D"
+
+elif model_name == "gr6j":
+    model = gr6j.GR6J()
+    CalibrationObject = gr6j.CalibrationGR6J
+    means = gr6j.GR6J_TMEAN
+    cov = gr6j.GR6J_TCOV
+    true2trans = gr6j.gr6j_true2trans
+    trans2true = gr6j.gr6j_trans2true
+    timestep = "D"
+
+elif model_name == "wapaba":
+    model = wapaba.WAPABA()
+    CalibrationObject = wapaba.CalibrationWAPABA
+    means = wapaba.WAPABA_TMEAN
+    cov = wapaba.WAPABA_TCOV
+    true2trans = wapaba.wapaba_true2trans
+    trans2true = wapaba.wapaba_trans2true
+    timestep = "MS"
+
+# Reciprocal transform
+trans_recip = transform.Reciprocal()
+trans_recip.nu = 1e-1 if timestep=="MS" else 1e-3
+
+# 10 years of warmup
+warmup = int(10*365.25) if timestep == "D" else 120
+
+# Load parameters
+if version == 1:
+    LOGGER.info(f"Retrieve parameter library from package")
 else:
     LOGGER.info(f"Retrieve parameter library from version {version-1}")
-    flib = fout.parent / f"calsac_v{version-1}"
-    lf = list(flib.glob(f"sacparams*v{version-1}.json"))
+    flib = fout.parent / f"calmodel_m{model_name}_v{version-1}"
+    lf = list(flib.glob(f"params_*_m{model_name}_v{version-1}.json"))
     nf = len(lf)
     tplib = np.zeros((nf, model.params.nval))
     #perf = np.zeros((nf, 2))
@@ -123,8 +159,7 @@ else:
         with f.open("r") as fo:
             p = json.load(fo)
         pv = np.array([p["params"][n] for n in model.params.names])
-        tplib[i, :] = sac15.sac15_true2trans(pv)
-        #perf[i, :] = [p["nse"], p["bias"]]
+        tplib[i, :] = true2trans(pv)
 
     means = np.mean(tplib, axis=0)
     cov = np.cov(tplib.T)
@@ -136,51 +171,54 @@ tplib = sutils.lhs_norm(nparamslib, means, cov)
 # Back transform
 plib = tplib * 0.
 for i in range(len(plib)):
-    plib[i, :] = sac15.sac15_trans2true(tplib[i, :])
+    plib[i, :] = trans2true(tplib[i, :])
 plib = np.clip(plib, model.params.mins, model.params.maxs)
 
 # Run calibration
 tbar = tqdm(enumerate(sites.iterrows()), total=len(sites), \
-                desc="Sac", disable=not progress)
+                desc="Calib", disable=not progress)
 for i, (siteid, row) in tbar:
     if not progress:
         LOGGER.info("dealing with {0} ({1}/{2})".format( \
             siteid, i, len(sites)))
 
-    fparams = fout / f"sacparams_{siteid}_v{version}.json"
+    fparams = fout / f"params_{siteid}_m{model_name}_v{version}.json"
     if fparams.exists() and not overwrite:
         continue
 
     # Get data
-    daily, _ = dset.get(siteid, "rainfall_runoff", "D")
-    dates = daily.index
-    obs = daily.loc[:, "runoff[mm/d]"].values
-    inputs = daily.loc[:, ["rain[mm/d]", "evap[mm/d]"]].values
+    data, _ = dset.get(siteid, "rainfall_runoff", timestep)
+    dates = data.index
+    obs = data.filter(regex="^runoff\[", axis=1).values.squeeze()
+    inputs = data.filter(regex="^(evap|rain)\[", axis=1).values.squeeze()
 
     start = max(0, np.where(~np.isnan(obs))[0].min()-warmup)
-    end = min(len(daily)-1, np.where(~np.isnan(obs))[0].max())
+    end = min(len(data)-1, np.where(~np.isnan(obs))[0].max())
     date_start = dates[start]
     date_end = dates[end]
     obs = obs[start:end]
     inputs = np.ascontiguousarray(inputs[start:end])
     ical = np.where(~np.isnan(obs) & (np.arange(len(obs))>warmup))[0]
 
-    if len(ical) < 365*10:
-        LOGGER.error(f"Record too short ({len(ical)} days). Skip")
+    min_length = 3650 if timestep == "D" else 120
+    if len(ical) < min_length:
+        LOGGER.error(f"Record too short ({len(ical)} {timestep}). Skip")
         continue
 
     # Calibrate on whole period
-    cal = sac15.CalibrationSAC15(warmup=warmup, nparamslib=nparamslib)
+    cal = CalibrationObject(warmup=warmup, nparamslib=nparamslib)
 
     # Set paramslib
     cal.paramslib = plib
 
     # Calibrate
-    final, _, _, _ = cal.workflow(obs, inputs, ical=ical)
+    final, _, _, _ = cal.workflow(obs, inputs, ical=ical, \
+                                    optimizer=fmin_bfgs)
 
     # Compute simple perfs NSE / bias
     o, s = obs[ical], cal.model.outputs[ical, 0]
     nse = metrics.nse(o, s)
+    nserecip = metrics.nse(o, s, trans=trans_recip)
     bias = metrics.bias(o, s)
 
     # Store
@@ -188,14 +226,17 @@ for i, (siteid, row) in tbar:
     dd = {
         "siteid": siteid, \
         "version": version, \
+        "model": model_name, \
         "warmup": warmup, \
         "nparamslib": nparamslib, \
         "nse": round(nse, 3), \
+        "nserecip": round(nserecip, 3), \
         "bias": round(bias, 3), \
         "params": params
     }
     with fparams.open("w") as fo:
         json.dump(dd, fo, indent=4)
+
 
 LOGGER.info("Process completed")
 
