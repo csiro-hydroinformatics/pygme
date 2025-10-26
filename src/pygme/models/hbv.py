6bbe1682a49eda16c693dceaf8eaddf0aae6fbcf
Julien Lerat <julien.lerat@csiro.au>
2025-10-27 08:17:05 +1100

    fix: fixed hbv style

--- src/pygme/models/hbv.py
+++ src/pygme/models/hbv.py
@@ -1,13 +1,32 @@
 import math
 import numpy as np
-import pandas as pd
 
 from hydrodiy.stat import sutils
 from hydrodiy.data.containers import Vector
-from pygme.model import Model, ParamsVector, UH
+from pygme.model import Model, ParamsVector
 from pygme.calibration import Calibration, CalibParamsVector, ObjFunBCSSE
 
 import c_pygme_models_hydromodels
+
+
+def hbv_params_trans2true(x):
+    return np.array([math.exp(x[0]),
+                     math.sinh(x[1]),
+                     math.exp(x[2]),
+                     0.49+math.exp(x[3]),
+                     math.sinh(x[4]),
+                     math.exp(x[5])
+                     ])
+
+
+def hbv_params_true2trans(x):
+    return np.array([math.log(x[0]),
+                     math.asinh(x[1]),
+                     math.log(x[2]),
+                     math.log(x[3]-0.49),
+                     math.asinh(x[4]),
+                     math.log(x[5])
+                     ])
 
 
 class HBV(Model):
@@ -15,16 +34,14 @@ class HBV(Model):
     def __init__(self):
 
         # Config vector
-        config = Vector(['nothing'],\
-                    [0], [0], [1])
+        config = Vector(['nothing'], [0], [0], [1])
 
         # params vector
-        vect = Vector(
-                    ['LPRAT', 'FC', 'BETA', 'K0', 'K1', 'K2', \
-                        'LSUZ', 'CPERC', 'BMAX', 'CROUTE'], \
-                    [0.9, 100, 3.3, 0.5, 9, 105, 50, 2, 10, 26.5], \
-                    [0, 0, 0, 0, 2, 30, 1, 0, 0, 0], \
-                    [1, 600, 20, 2, 30 ,250, 100, 8, 30, 50])
+        vect = Vector(['LPRAT', 'FC', 'BETA', 'K0', 'K1', 'K2',
+                       'LSUZ', 'CPERC', 'BMAX', 'CROUTE'],
+                      [0.9, 100, 3.3, 0.5, 9, 105, 50, 2, 10, 26.5],
+                      [0, 0, 0, 0, 2, 30, 1, 0, 0, 0],
+                      [1, 600, 20, 2, 30, 250, 100, 8, 30, 50])
 
         params = ParamsVector(vect)
 
@@ -33,98 +50,77 @@ class HBV(Model):
 
         # Model
         super(HBV, self).__init__('HBV',
-            config, params, states, \
-            ninputs=2, \
-            noutputsmax=13)
+                                  config,
+                                  params,
+                                  states,
+                                  ninputs=2,
+                                  noutputsmax=13)
 
-        self.outputs_names = ['Q', 'BRT', 'DQ', \
-                    'DMOIST', 'ETA', 'Q0', 'Q1', 'Q2', 'QG', 'SUM', \
-                    'MOIST', 'SUZ', 'SLZ']
-
+        self.outputs_names = ['Q', 'BRT', 'DQ',
+                              'DMOIST', 'ETA', 'Q0',
+                              'Q1', 'Q2', 'QG', 'SUM',
+                              'MOIST', 'SUZ', 'SLZ'
+                              ]
 
     def initialise_fromdata(self):
         ''' Initialisation of HBV using
             * Production store:
             * Routing store:
         '''
-        LSUZ = self.params.LSUZ
-        K1 = self.params.K1
-        K2 = self.params.K2
-        K3 = self.params.K3
-
         MOIST0 = 0.
         SUZ0 = 0.
         SLZ0 = 0.
-
         self.initialise([MOIST0, SUZ0, SLZ0])
-
 
     def run(self):
         # Run hbv c code
-        ierr = c_pygme_models_hydromodels.hbv_run(
-            self.istart, self.iend, \
-            self.params.values, \
-            self.inputs, \
-            self.states.values, \
-            self.outputs)
-
+        ierr = c_pygme_models_hydromodels.hbv_run(self.istart,
+                                                  self.iend,
+                                                  self.params.values,
+                                                  self.inputs,
+                                                  self.states.values,
+                                                  self.outputs)
         if ierr > 0:
-            raise ValueError(('c_pygme_models_hydromodels.hbv_run' +
-                ' returns {0}').format(ierr))
+            errmsg = "c_pygme_models_hydromodels.hbv_run"\
+                     + f" returns {ierr}."
+            raise ValueError(errmsg)
 
 
 class CalibrationHBV(Calibration):
 
-    def __init__(self, objfun=ObjFunBCSSE(0.2), \
-                    warmup=5*365, \
-                    timeit=False, \
-                    fixed=None, \
-                    objfun_kwargs={}):
+    def __init__(self,
+                 objfun=ObjFunBCSSE(0.2),
+                 warmup=5*365,
+                 timeit=False,
+                 fixed=None,
+                 objfun_kwargs={}):
 
         # Input objects for Calibration class
         model = HBV()
         params = model.params
 
-        # TODO
-        #trans2true = lambda x: np.array([
-        #                math.exp(x[0]), \
-        #                math.sinh(x[1]), \
-        #                math.exp(x[2]), \
-        #                0.49+math.exp(x[3]), \
-        #                math.sinh(x[4]), \
-        #                math.exp(x[5])
-        #            ])
+        cp = Vector(['tLPRAT', 'tFC', 'tBETA', 'tK0', 'tK1', 'tK2',
+                     'tLSUZ', 'tCPERC', 'tBMAX', 'tCROUTE'],
+                    mins=hbv_params_true2trans(params.mins),
+                    maxs=hbv_params_true2trans(params.maxs),
+                    defaults=hbv_params_true2trans(params.defaults))
 
-        #true2trans = lambda x: np.array([
-        #                math.log(x[0]), \
-        #                math.asinh(x[1]), \
-        #                math.log(x[2]), \
-        #                math.log(x[3]-0.49), \
-        #                math.asinh(x[4]), \
-        #                math.log(x[5])
-        #            ])
-
-        cp = Vector(['tLPRAT', 'tFC', 'tBETA', 'tK0', 'tK1', 'tK2', \
-                        'tLSUZ', 'tCPERC', 'tBMAX', 'tCROUTE'], \
-                mins=true2trans(params.mins),
-                maxs=true2trans(params.maxs),
-                defaults=true2trans(params.defaults))
-
-        calparams = CalibParamsVector(model, cp, \
-            trans2true=trans2true, \
-            true2trans=true2trans,\
-            fixed=fixed)
+        calparams = CalibParamsVector(model, cp,
+                                      trans2true=hbv_params_trans2true,
+                                      true2trans=hbv_params_true2trans,
+                                      fixed=fixed)
 
         # Build parameter library from
         # MVT norm in transform space
         # TODO !!!!!!!
         mean = [5.8, -0.78, 3.39, 0.86, 0., 3.]
-        cov = [[1.16, 0.2, -0.15, -0.07, 0., 0.],\
-                [0.2, 1.79, -0.24, -0.149, 0., 0.],\
-                [-0.15, -0.24, 1.68, -0.16, 0., 0.],\
-                [-0.07, -0.149, -0.16, 0.167, 0., 0.],\
-                [0., 0., 0., 0., 1., 0.], \
-                [0., 0., 0., 0., 0., 1.]]
+        cov = [[1.16, 0.2, -0.15, -0.07, 0., 0.],
+               [0.2, 1.79, -0.24, -0.149, 0., 0.],
+               [-0.15, -0.24, 1.68, -0.16, 0., 0.],
+               [-0.07, -0.149, -0.16, 0.167, 0., 0.],
+               [0., 0., 0., 0., 1., 0.],
+               [0., 0., 0., 0., 0., 1.]
+               ]
         tplib = sutils.lhs_norm(5000, mean, cov)
         tplib = np.clip(tplib, calparams.mins, calparams.maxs)
         plib = tplib * 0.
@@ -136,12 +132,10 @@ class CalibrationHBV(Calibration):
         initial_kwargs = {}
 
         # Instanciate calibration
-        super(CalibrationHBV, self).__init__(calparams, \
-            objfun=objfun, \
-            warmup=warmup, \
-            timeit=timeit, \
-            paramslib=plib, \
-            objfun_kwargs=objfun_kwargs,\
-            initial_kwargs=initial_kwargs())
-
-
+        super(CalibrationHBV, self).__init__(calparams,
+                                             objfun=objfun,
+                                             warmup=warmup,
+                                             timeit=timeit,
+                                             paramslib=plib,
+                                             objfun_kwargs=objfun_kwargs,
+                                             initial_kwargs=initial_kwargs())
